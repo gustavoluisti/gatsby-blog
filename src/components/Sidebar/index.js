35fc99721e771bcd357079c6f33b9c1ba8378ea7
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-08-12 13:57:33 -0300

    Atualizacao

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Profile from "../Profile"
+
+import * as S from "./style"
+
+const Sidebar = () => (
+  <S.SidebarWrapper>
+    <Profile />
+  </S.SidebarWrapper>
+)
+
+export default Sidebar
